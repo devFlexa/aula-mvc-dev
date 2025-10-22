85b67d95ecbc8ea0b34cb9cbd3c406b073b489d8
devFlexa <thiagolevantmkt@gmail.com>
2025-10-22 06:11:05 -0300

    finalização

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using aula_mvc.Context;
 using aula_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,17 +10,103 @@ namespace aula_mvc.Controllers
 {
     public class ContatoController : Controller
     {
+        // public IActionResult Index()
+        // {
+
+        //     var contatos = new List<Contato>
+        //     {
+        //         new Contato { Id = 1, Nome = "Maria", Telefone = "1111-1111", Ativo = true },
+        //         new Contato { Id = 2, Nome = "João", Telefone = "2222-2222", Ativo = false },
+        //         new Contato { Id = 3, Nome = "Ana", Telefone = "3333-3333", Ativo = true }
+        //     };
+
+        //     return View(contatos);
+        // }
+
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-
-            var contatos = new List<Contato>
-            {
-                new Contato { Id = 1, Nome = "Maria", Telefone = "1111-1111", Ativo = true },
-                new Contato { Id = 2, Nome = "João", Telefone = "2222-2222", Ativo = false },
-                new Contato { Id = 3, Nome = "Ana", Telefone = "3333-3333", Ativo = true }
-            };
-
+            var contatos = _context.Contatos.ToList();
             return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Migrations/20251022091023_criacaoTabelaContato2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aula_mvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class criacaoTabelaContato2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
